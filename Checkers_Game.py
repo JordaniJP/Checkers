f6439c67af4c4beee787b3c240c1541e7d06bb2f
JordaniJP <35529785+JordaniJP@users.noreply.github.com>
2018-03-01 22:51:21 -0500

    Fixed problem when 2 different windows would pop up

--- Checkers_Game.py
+++ Checkers_Game.py
@@ -1,21 +1,25 @@
 from Tkinter import *
 
-root = Tk()
-def checkerboard(can):
-    w=can.winfo_width()
-    h=can.winfo_height()
-    cellwidth = w/10
-    cellheight=h/10
-    for row in range(10):
-        for col in range(10):
-            if (row+col)%2==0:
-                can.create_rectangle(col*cellwidth,row*cellheight,(col+1)*cellwidth,(row+1)*cellheight,fill='black')
-
-
 window=Tk()
-thecanvas = Canvas(window, width=500,height=500)
+
+
+
+def checkerboard(board):
+    w=board.winfo_width()
+    h=board.winfo_height()
+    boxW = w/10
+    boxH=h/10
+    for row in range(8):
+        for col in range(8):
+            if (row+col)%2==0:
+                board.create_rectangle(col*boxW,row*boxH,(col+1)*boxW,(row+1)*boxH,fill='red')
+            
+
+
+
+
+thecanvas = Canvas(window, width=700,height=700)
 thecanvas.grid(row=0,column=0)
 window.update_idletasks()
 checkerboard(thecanvas)
 window.mainloop()
-root.mainloop()
